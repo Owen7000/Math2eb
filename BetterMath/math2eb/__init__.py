846fd6ef0aa59cd5e3b76ff2d17a52c018cbdf69
Owen <77976026+Owen7000@users.noreply.github.com>
2022-11-02 17:22:08 +0000

    many things, such as project name, and description etc, nothing interesting though ¯\_(ツ)_/¯

---
+++ BetterMath/math2eb/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '0.1.1'
+
+class base:
+  def add(a, b):
+    """Returns the sum of A+B"""
+    return a + b
+
+  def Subtract(a, b):
+    """Returns the sum of A-B"""
+    return a - b
+
+  def Multiply(a, b):
+    """Returns the sum of A*B"""
+    return a * b
+
+  def Divide(a, b):
+    """Returns the sum of A/B"""
+    return a/b
