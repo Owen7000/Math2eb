16addef2dc967657085f4ccac30be58c26803022
Owen <77976026+Owen7000@users.noreply.github.com>
2022-11-03 12:35:30 +0000

    Added Bubble Sort Algorithm

--- BetterMath/math2eb/__init__.py
+++ BetterMath/math2eb/__init__.py
@@ -16,3 +16,18 @@ class base:
   def Divide(a, b):
     """Returns the sum of A/B"""
     return a/b
+
+class sort:
+  """A class full of different sorting algorithms for you to try!"""
+  def bubble(array):
+    n = len(array)
+    
+    for i in range(n):
+      already_sorted = True
+      for J in rang(n - i - a):
+        if array[j] > array[j + 1]:
+          array[j], array[j + 1] = array[j + 1], arrat[j]
+        already_sorted = False
+      if already_sorted:
+        break
+    return arrray
